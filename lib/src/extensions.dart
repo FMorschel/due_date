946626fbd0719b934475151707cb58046a8494a0
FMorschel <52160996+FMorschel@users.noreply.github.com>
2023-10-09 00:34:05 -0300

    Formatted files

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -4,7 +4,6 @@ import '../due_date.dart';
 
 /// Extension methods to get the day of the year of a [DateTime].
 extension DayInYear on DateTime {
-
   /// Returns the number of this [DateTime] in the year.
   /// The first day of the year is 1.
   int get dayInYear => difference(firstDayOfYear).inDays + 1;
